a91b327865805f086365fb424c1cdee74ba0d887
Kutluhan Buyukburc <kbuyukburc@gmail.com>
2017-05-04 23:23:39 +0300

    Add files via upload

---
+++ dictionary/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(-2, -1)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(336, 443)
+        Me.WebBrowser1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 445)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 10000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(335, 459)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ dictionary/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.ComponentModel
+Imports System.Threading.Thread
+Public Class Form1
+    Dim search, searchnew, index As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        searchnew = Clipboard.GetText
+        search = searchnew
+        Me.Location = New System.Drawing.Point(Screen.PrimaryScreen.Bounds.Width - Me.Width,
+Screen.PrimaryScreen.Bounds.Height - Me.Height - 100)
+        WebBrowser1.ScriptErrorsSuppressed = True
+        Timer1.Enabled = Enabled
+        ' Me.Visible = False
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Me.Visible = False
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        Me.Visible = False
+        'Me.Close()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        On Error Resume Next
+        ' On Error Resume Next
+        searchnew = Clipboard.GetText
+        Label1.Text = searchnew
+        If Clipboard.GetText = "Xclose" Then
+            Application.Exit()
+        End If
+        If searchnew <> search Then
+            search = searchnew
+            Me.WebBrowser1.Navigate("http://tureng.com/tr/turkce-ingilizce/" + Replace(search, " ", "%20"))
+            Do Until WebBrowser1.ReadyState = WebBrowserReadyState.Complete Or Clipboard.GetText = "Xclose"
+                Application.DoEvents()
+            Loop
+            index = WebBrowser1.DocumentText
+            If index.Contains("Sanırız yanlış oldu, doğrusu şunlar olabilir mi?") Then
+                ' MessageBox.Show("yes")
+            Else
+                Timer1.Enabled = False
+                Me.Visible = True
+                Me.Refresh()
+                WebBrowser1.Document.Window.ScrollTo(0, 220)
+                Timer2.Enabled = Enabled
+
+            End If
+        End If
+
+
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        If Clipboard.GetText = "Xclose" Then
+            'Application.Exit()
+            ' Me.Close()
+        Else
+            e.Cancel = True
+            Timer2.Enabled = False
+            Timer1.Enabled = True
+            Me.Visible = False
+        End If
+    End Sub
+End Class
